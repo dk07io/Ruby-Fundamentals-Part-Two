92a02a86fc09d6f14284719dde928118da53c400
Daniel Kim <dk07io@Daniels-MacBook-Pro-3.local>
2015-01-21 16:59:38 -0500

    Removed a method

--- exercise6.rb
+++ exercise6.rb
@@ -1,20 +1,20 @@
 #exercise6.rb
-def shopping_list
-	grocery_list = ["carrots","toilet paper", "apples","salmon"]
-	grocery_list << "rice"
-	grocery_list.delete_at(3)
-	grocery_list.sort
-end
-	shopping_list.each{|x| puts "*#{x}"}
-	puts "Total item on Shopping List = #{shopping_list.length}"
 
-def banana_check
-	if shopping_list.include?("banana")
+grocery_list = ["carrots","toilet paper", "apples","salmon"]
+grocery_list << "rice"
+grocery_list.delete_at(3)
+grocery_list.sort
+
+grocery_list.each{|x| puts "*#{x}"}
+puts "Total item on Shopping List = #{grocery_list.length}"
+
+def banana_check(a_list)
+	if a_list.include?("banana")
 		puts "You need to pick up bananas."
 	else
 		puts "You don't need to pick up bananas today!"
 	end
 end
 
- puts banana_check
- puts "The second item on the Shopping List is #{shopping_list[1]}."
+ puts banana_check(grocery_list)
+ puts "The second item on the Shopping List is #{grocery_list[1]}."
